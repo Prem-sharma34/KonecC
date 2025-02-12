679f83348095528cfb55b0952aff7b99f4bdeaf4
suryakapoor124 <suryakapoor124@gmail.com>
2025-02-12 22:41:56 +0530

    just a starter vite+react app

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,23 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <div>
+    <h2>konecC</h2>
+    <Tagline />
+    </div>
+    </>
+  ) 
+}
+function Tagline(){
+  return(
+    <>
+    <h4>Connecting People</h4>
+    </>
+  )
+}
+
+export default App
